da9429718ed27112c78ff816898d20a5134dc622
jefcodev <jeffersonpinchao@hotmail.com>
2023-01-21 18:42:45 -0500

    Validar JWT

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,146 +1,132 @@
-const { response } = require('express');
+const { response } = require("express");
 
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 const Usuario = require("../models/usuario");
+const { generarJWT } = require("../helpers/jwt.");
 
 const getUsuarios = async (req, res) => {
-    const usuarios = await Usuario.find({}, "nombre email google password");
+  const usuarios = await Usuario.find({}, "nombre email google password");
 
-    res.json({
-        usuarios,
-    });
+  res.json({
+    usuarios,
+  });
 };
 
 const crearUsuario = async (req, res = response) => {
-    const { password, email } = req.body;
+  const { password, email } = req.body;
 
-    try {
-        const existEmail = await Usuario.findOne({ email });
-        if (existEmail) {
-            return res.status(400).json({
-                ok: "false",
-                msg: "El correo ya esta en uso",
-            });
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: "false",
-            msg: "Error inesperado... revisar logs",
-        });
+  try {
+    const existEmail = await Usuario.findOne({ email });
+    if (existEmail) {
+      return res.status(400).json({
+        ok: "false",
+        msg: "El correo ya esta en uso",
+      });
     }
-
-    const usuario = new Usuario(req.body);
-
-    //Encriptar contraseña
-    const salt = bcrypt.genSaltSync();
-    usuario.password = bcrypt.hashSync(password, salt);
-
-
-
-    // espera a que la promesa temine
-    // guardar ususario
-    await usuario.save();
-
-    res.json({
-        ok: true,
-        usuario,
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: "false",
+      msg: "Error inesperado... revisar logs",
     });
+  }
+
+  const usuario = new Usuario(req.body);
+
+  //Encriptar contraseña
+  const salt = bcrypt.genSaltSync();
+  usuario.password = bcrypt.hashSync(password, salt);
+
+  // espera a que la promesa temine
+  // guardar ususario
+  await usuario.save();
+
+  //Generar Token
+  const token = await generarJWT(usuario.id);
+  res.json({
+    ok: true,
+    usuario,
+    token,
+  });
 };
 
-
-
 const actualizarUsuario = async (req, res = response) => {
+  const uid = req.params.id;
 
-    const uid = req.params.id;
+  try {
+    // buscando el usuario por id
+    const usuarioDB = await Usuario.findById(uid);
 
-
-    try {
-        // buscando el usuario por id
-        const usuarioDB = await Usuario.findById(uid);
-
-        if (!usuarioDB) {
-            return res.status(404).json({
-                ok: false,
-                msg: 'No existe un usuario por ese id'
-            });
-        }
-
-
-
-        /*Actualización*/
-
-        //Extraer datos del objetp
-        const {password,google,email,...campos} = req.body;
-
-        if (usuarioDB.email !== email) {
-            const existEmail = await Usuario.findOne({ email });
-            if (existEmail) {
-                return res.status(400).json({
-                    ok: false,
-                    msg: 'El correo ya existe....'
-                })
-
-            }
-        }
-        campos.email = email;
-        const usuarioActualizado = await Usuario.findByIdAndUpdate(uid, campos, { new: true });
-        res.json({
-            ok: true,
-            msg: usuarioActualizado
-        })
-
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error insesperado'
-
-        })
-
+    if (!usuarioDB) {
+      return res.status(404).json({
+        ok: false,
+        msg: "No existe un usuario por ese id",
+      });
     }
 
-}
+    /*Actualización*/
 
+    //Extraer datos del objetp
+    const { password, google, email, ...campos } = req.body;
 
-const deleteUsuario = async (req, res = response) =>{
-    const uid = req.params.id;
-    try {
-
-        // buscando el usuario por id
-        const usuarioDB = await Usuario.findById(uid);
-
-        if (!usuarioDB) {
-            return res.status(404).json({
-                ok: false,
-                msg: 'No existe un usuario por ese id'
-            });
-        }
-
-        await Usuario.findByIdAndDelete(uid);
-        
-        
-        res.json({
-            ok: true,
-            msg: 'Usuario eliminado correctamente'
+    if (usuarioDB.email !== email) {
+      const existEmail = await Usuario.findOne({ email });
+      if (existEmail) {
+        return res.status(400).json({
+          ok: false,
+          msg: "El correo ya existe....",
         });
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            ok:false,
-            msg:'Error insesperdao revisar el log'
-        });
-        
+      }
+    }
+    campos.email = email;
+    const usuarioActualizado = await Usuario.findByIdAndUpdate(uid, campos, {
+      new: true,
+    });
+    res.json({
+      ok: true,
+      msg: usuarioActualizado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error insesperado",
+    });
+  }
+};
+
+const deleteUsuario = async (req, res = response) => {
+  const uid = req.params.id;
+  try {
+    // buscando el usuario por id
+    const usuarioDB = await Usuario.findById(uid);
+
+    if (!usuarioDB) {
+      return res.status(404).json({
+        ok: false,
+        msg: "No existe un usuario por ese id",
+      });
     }
 
-}
+    await Usuario.findByIdAndDelete(uid);
+
+    res.json({
+      ok: true,
+      msg: "Usuario eliminado correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error insesperdao revisar el log",
+    });
+  }
+};
 
 module.exports = {
-    getUsuarios,
-    crearUsuario,
-    actualizarUsuario,
-    deleteUsuario
+  getUsuarios,
+  crearUsuario,
+  actualizarUsuario,
+  deleteUsuario,
 };
--- routes/usuario.js
+++ routes/usuario.js
@@ -2,16 +2,18 @@ const {Router} = require('express');
 const {body} = require('express-validator');
 const {validarCampos} = require('../middlewares/validar-campos')
 
-const {getUsuarios, crearUsuario, actualizarUsuario, deleteUsuario} = require('../controllers/usuario')
+const {getUsuarios, crearUsuario, actualizarUsuario, deleteUsuario} = require('../controllers/usuario');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 // Ruta de usuarios /api/usuarios
 
 const router = Router();
 
 
-router.get('/', getUsuarios);
+router.get('/',validarJWT ,getUsuarios);
 router.post('/',
 [
+    
     body('nombre', 'El nombre es obligatorio').not().isEmpty(),
     body('password','El password es obligatorio').not().isEmpty(),
     body('email', 'El correo es obligatorio').isEmail(),
@@ -21,6 +23,7 @@ router.post('/',
 
 router.put('/:id', 
     [
+        validarJWT,
         body('nombre', 'El nombre es obligatorio').not().isEmpty(),
         body('email','El email es obligatorio').isEmail(),
         body('role', 'El role es obligatorio').not().isEmpty(),
@@ -29,6 +32,6 @@ router.put('/:id',
     actualizarUsuario
 );
 
-router.delete('/:id', deleteUsuario)
+router.delete('/:id', validarJWT,deleteUsuario)
 
 module.exports = router;
